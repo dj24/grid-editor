7067005f2abdb8b0993c699394fbcc4c97857130
Daniel Jackson <dan@quickfiredigital.com>
2019-08-02 16:04:01 +0100

    attempted size percentage fix

--- grid-editor-react/src/components/ImageBlock.js
+++ grid-editor-react/src/components/ImageBlock.js
@@ -3,30 +3,51 @@ import { Rnd } from "react-rnd";
 import CKEditor from "@ckeditor/ckeditor5-react";
 import InlineEditor from "@ckeditor/ckeditor5-build-inline";
 
-function ImageBlock(props) {
-  const style = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundImage: "url(" + props.src + ")",
-    backgroundSize: "cover",
-    backgroundPosition: "center"
-  };
+class ImageBlock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      width:200,
+    };
+  }
+  render(){
+    const style = {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundImage: "url(" + this.props.src + ")",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      width: this.state.width
+    };
 
-  return (
-    <Rnd
-      className="animated fadeIn"
-      style={style}
-      dragGrid={[20, 20]}
-      resizeGrid={[20, 20]}
-      default={{
-        x: 500,
-        y: 200,
-        width: 200,
-        height: 200
-      }}
-    />
-  );
+    return (
+      <Rnd
+        bounds={"window"}
+
+        onResize={(e, direction, ref, delta, position) => {
+          let width = ref.offsetWidth;
+          let container = window.innerWidth;
+          let percentage  = Math.round((width / container) * 10000)/100;
+          console.log("PERCENT: " + percentage);
+          this.setState({
+            width: percentage + "%"
+          });
+
+        }}
+        className="animated fadeIn"
+        style={style}
+        dragGrid={[20, 20]}
+        resizeGrid={[20, 20]}
+        default={{
+          x: 500,
+          y: 200,
+          width: 200,
+          height: 200
+        }}
+      />
+    );
+  }
 }
 
 export default ImageBlock;
